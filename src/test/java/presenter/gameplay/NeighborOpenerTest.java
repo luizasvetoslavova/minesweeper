26f891e5b9619426b287199d9dbcfbba7a2f1f9e
Luiza Svetoslavova <luizasvetoslavova16@gmail.com>
2023-06-24 23:51:15 +0300

    finish neighbor opener test

--- src/test/java/presenter/gameplay/NeighborOpenerTest.java
+++ src/test/java/presenter/gameplay/NeighborOpenerTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class NeighborOpenerTest {
@@ -25,24 +27,64 @@ public class NeighborOpenerTest {
 
     @Test
     void testOpenNeighbors_WhenOpenedCellIsEmpty_ThenOpenAllNeighbors() {
-        Cell zero = getFirstEmptyCell();
-        opener.openNeighbors(zero);
-        getAllNeighbors(zero).forEach(neighbor -> Assertions.assertEquals(neighbor.getCellStatus(), CellStatus.OPENED));
+        Cell empty = getFirstEmptyCell();
+        opener.openNeighbors(empty);
+        getAllNeighbors(empty).forEach(neighbor -> Assertions.assertEquals(neighbor.getCellStatus(), CellStatus.OPENED));
     }
 
     @Test
     void testOpenNeighbors_WhenOpenedCellIsEmpty_ThenOpenAllConnectedEmptyNeighbors() {
+        opener.openNeighbors(getFirstEmptyCell());
 
+        Arrays.stream(matrix.getCells()).forEach(array -> Arrays.stream(array).forEach(cell -> {
+            if (cell.getDigit() == 0 && cell.getCellStatus().equals(CellStatus.OPENED)) {
+                getAllNeighbors(cell).forEach(neighbor -> {
+                    if (neighbor.getDigit() == 0) Assertions.assertEquals(neighbor.getCellStatus(), CellStatus.OPENED);
+                });
+            }
+        }));
     }
 
     @Test
     void testIsNeighbor_WhenComparingNeighbors_ThenReturnTrue() {
+        Cell randomCell = getRandomCell();
+        Cell neighbor = null;
 
+        outerloop: for (int line1 = 0; line1 < matrix.getCells().length; line1++) {
+            for (int col1 = 0; col1 < matrix.getCells()[line1].length; col1++) {
+                Cell potentialNeighbor = matrix.getCells()[line1][col1];
+                if (opener.isNeighbor(randomCell, line1, col1)) {
+                    neighbor = potentialNeighbor;
+                    break outerloop;
+                }
+            }
+        }
+        Assertions.assertTrue(getAllNeighbors(randomCell).contains(neighbor));
     }
 
     @Test
     void testIsNeighbor_WhenComparingNonNeighbors_ThenReturnFalse() {
+        Cell randomCell = getRandomCell();
+        Cell nonNeighbor = null;
 
+        outerloop:
+        for (int line1 = 0; line1 < matrix.getCells().length; line1++) {
+            for (int col1 = 0; col1 < matrix.getCells()[line1].length; col1++) {
+                Cell potentialNonNeighbor = matrix.getCells()[line1][col1];
+                if (!opener.isNeighbor(randomCell, line1, col1)) {
+                    nonNeighbor = potentialNonNeighbor;
+                    break outerloop;
+                }
+            }
+        }
+        Assertions.assertFalse(getAllNeighbors(randomCell).contains(nonNeighbor));
+    }
+
+    private Cell getRandomCell() {
+        Random random = new Random();
+        int line = random.nextInt(matrix.getCells().length);
+        int col = random.nextInt(matrix.getCells()[line].length);
+        return matrix.getCells()[line][col];
     }
 
     private ArrayList<Cell> getAllNeighbors(Cell cell) {
@@ -51,7 +93,6 @@ public class NeighborOpenerTest {
         for (int line = 0; line < matrix.getCells().length; line++) {
             for (int col = 0; col < matrix.getCells()[line].length; col++) {
                 Cell current = matrix.getCells()[line][col];
-
                 if (opener.isNeighbor(cell, line, col) && !cell.isBomb()
                         && !current.getCellStatus().equals(CellStatus.FLAGGED)) {
                     cells.add(current);
