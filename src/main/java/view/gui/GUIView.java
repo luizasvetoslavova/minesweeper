fba76d6cd38daf10c38a2825b2fb51c934cc1536
Luiza Svetoslavova <luizasvetoslavova16@gmail.com>
2023-06-26 23:01:55 +0300

    do home page

--- src/main/java/view/gui/GUIView.java
+++ src/main/java/view/gui/GUIView.java
@@ -5,12 +5,10 @@ import java.awt.*;
 
 public class GUIView {
     private final JFrame frame;
-    private final JPanel panel;
+    private JPanel mainPanel;
 
     public GUIView() {
         this.frame = new JFrame();
-        this.panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        frame.setVisible(true);
     }
 
     public void initJFrame() {
@@ -20,36 +18,40 @@ public class GUIView {
         frame.setLocation(200, 80);
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        frame.add(mainPanel);
     }
 
     public void showRules() {
-        JLabel rules = new JLabel("<html>" + "<br>" + "<div style='text-align: center;'>" +
+        JLabel rules = new JLabel("<html>" + "<br>" + "<br>" + "<br>" + "<br>" + "<div style='text-align: " +
+                "center;'>" +
                 "Welcome to Minesweeper!" + "<br>" + "<br>" + "Rules:" + "<br>" +
-                "~ The number shown on an unlocked cell is the number of model.mines adjacent to it." + "<br>" +
-                "~ You have to flag all the model.mines and not unlock on a single one, or else you lose and the game ends." +
+                "1. The number shown on an unlocked cell is the number of model.mines adjacent to it." + "<br>" +
+                "2. You have to flag all the model.mines and not unlock on a single one, or else you lose and " +
+                "the game ends." +
                 "<br>" + "You can start by clicking at any random cell." + "<br>" + "Signs:" + "<br>" +
                 "◽ - Empty cell. There are no bombs near it." + "<br>" + "⬛ - Bomb." + "<br>" +
-                "⬜ - Unopened cell." + "<br>" + "⛳ - Flag." + "<br>" + "<br>" + "Pick your level:" +
-                "<br>" + "<br>" + "</div> </html>");
+                "⬜ - Unopened cell." + "<br>" + "⛳ - Flag." + "<br>" + "<br>" + "Pick your level:" + "</div> </html>");
 
         rules.setFont(new Font(rules.getFont().getName(), Font.PLAIN, 17));
-        panel.add(rules);
-        frame.add(panel);
+        JPanel rulesPanel = new JPanel();
+        rulesPanel.add(rules);
+        mainPanel.add(rulesPanel);
     }
 
     public void addButtons() {
-        JButton easyButton = new JButton("EASY");
-        JButton mediumButton = new JButton("MEDIUM");
-        JButton hardButton = new JButton("HARD");
-        JButton expertButton = new JButton("EXPERT");
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(new JButton("EASY"));
+        buttonPanel.add(new JButton("MEDIUM"));
+        buttonPanel.add(new JButton("HARD"));
+        buttonPanel.add(new JButton("EXPERT"));
 
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(Box.createVerticalStrut(10));
+        int topPadding = 50;
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(topPadding, 100, 200, 100));
 
-        panel.add(easyButton);
-        panel.add(mediumButton);
-        panel.add(hardButton);
-        panel.add(expertButton);
-        frame.add(panel);
+        mainPanel.add(buttonPanel);
+        frame.add(mainPanel);
     }
 }
